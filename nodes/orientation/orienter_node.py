dfd6c4bd9a573242e41b9301bdda98933c92cbc5
lab <josephvalentine40@gmail.com>
2017-01-31 18:11:10 -0700

    Tried to implement orienter. One of the robots now spins. I don't know if that's an improvement or not.

--- nodes/orientation/orienter_node.py
+++ nodes/orientation/orienter_node.py
@@ -4,64 +4,65 @@ from geometry_msgs.msg import Twist, Pose2D
 # from std_srvs.srv import Trigger, TriggerResponse
 
 _team_side = 'home'
+_home_1 = Pose2D()
+_home_2 = Pose2D()
+_away_1 = Pose2D()
+_away_2 = Pose2D()
+
+def _handle_home_1(msg):
+   _home_1 = msg
+
+def _handle_home_2(msg):
+   _home_2 = msg
+
+def _handle_away_1(msg):
+   _away_1 = msg
+
+def _handle_away_2(msg):
+   _away_2 = msg
+
 
 def main():
     global _team_side
     param_name = rospy.search_param('team_side')
     _team_side = rospy.get_param(param_name, 'home')
-    
+
     rospy.init_node('orienter', anonymous=False)
 
     # Sub/Pub
-    rospy.Subscriber('vision/away1', RobotState, _handle_robot_state)
-    rospy.Subscriber('desired_position', Pose2D, _handle_desired_position)
-    pub = rospy.Publisher('vel_cmds', Twist, queue_size=10)
-    pub_PIDInfo = rospy.Publisher('pidinfo', PIDInfo, queue_size=10)
+    rospy.Subscriber('vision/home1', Pose2D, _handle_home_1)
+    rospy.Subscriber('vision/home2', Pose2D, _handle_home_2)
+    rospy.Subscriber('vision/away1', Pose2D, _handle_away_1)
+    rospy.Subscriber('vision/away2', Pose2D, _handle_away_2)
 
-    # Services
-    # rospy.Service('/controller/toggle', Trigger, _toggle)
+    pub_us_1 = rospy.Publisher('orienter/us1', Pose2D, queue_size=10)
+    pub_us_2 = rospy.Publisher('orienter/us2', Pose2D, queue_size=10)
+    pub_them_1 = rospy.Publisher('orienter/them1', Pose2D, queue_size=10)
+    pub_them_2 = rospy.Publisher('orienter/them2', Pose2D, queue_size=10)
 
-    # Get the correct PID stuff
-    gains = rospy.get_param('gains') # returns as a dict
-    # {'x': {'P': 0, 'I': 0, 'D': 0}, ... }
-
-    # initialize the controller
-    Controller.init(gains)
-
+    ctrl_period = 100
+    
     rate = rospy.Rate(int(1/_ctrl_period))
     while not rospy.is_shutdown():
 
-        global _ctrl_on
+         if _team_side == 'home':
+            # publish
+            pub_us_1.publish(_home_1)
+            pub_us_2.publish(_home_2)
+            pub_them_1.publish(_away_1)
+            pub_them_2.publish(_away_2)
+         else:
+            # flip coordinate system
+            for msg in [_home_1, _home_2, _away_1, _away_2]:
+               msg.x = -msg.x
+               msg.y = -msg.y
+               msg.theta = msg.theta + 180 % 360
 
-        if _ctrl_on:
-            (vx, vy, w) = Controller.update(_ctrl_period, _xhat, _yhat, _thetahat)
-
-            if _team_side == 'away':
-               # flip coordinate system
-               vx = -vx
-               vy = -vy
-               w  = w + 180 % 360
-            
-            # Publish Velocity Commands
-            msg = Twist()
-            msg.linear.x = vx
-            msg.linear.y = vy
-            msg.angular.z = w
-            pub.publish(msg)
-
-            # # Publish PID Info
-            # msg = PIDInfo()
-            # msg.error.x = Controller.PID_x.error_d1
-            # msg.error.y = Controller.PID_y.error_d1
-            # msg.error.theta = Controller.PID_theta.error_d1
-            # set_point = Controller.get_commanded_position()
-            # msg.desired.x = set_point[0]
-            # msg.desired.y = set_point[1]
-            # msg.desired.theta = set_point[2]
-            # msg.actual.x = _xhat
-            # msg.actual.y = _yhat
-            # msg.actual.theta = _thetahat
-            # pub_PIDInfo.publish(msg)
+            # publish
+            pub_us_1.publish(_away_1)
+            pub_us_2.publish(_away_2)
+            pub_them_1.publish(_home_1)
+            pub_them_2.publish(_home_2)
 
 
         rate.sleep()
