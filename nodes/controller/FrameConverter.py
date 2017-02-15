c7066a2fba84203c6e6ae1fa6d45c7e6815b7418
steven93 <stwhit93@gmail.com>
2017-02-14 17:42:42 -0700

    FrameConverter.py updated to convert world velocities to body velocities.

--- nodes/controller/FrameConverter.py
+++ nodes/controller/FrameConverter.py
@@ -1,24 +1,16 @@
 import math
 from geometry_msgs.msg import Pose2D
 
-def _destination_world_to_body_coordinates(dest, curPos):
-    """ Takes the destination of the robot in world coordinates and converts it
-    a position in the robot frame of reference.
-    dest is a Pose2D representing the world coordinates of where the robot needs
-    to go.
-    curPos is a Pose2D representing where the robot currently is in world
-    coordinates.
-    Returns a Pose2D representing where the robot should go in its own frame of
-    reference.
+def _convert_world_to_body_velocities(vx_w, vy_w, curAngle):
+    """ Converts world velocities to body velocities.
+    vx_w is the world x velocity.
+    vy_w is the world y velocity.
+    curAngle is the current bearing of the robot in the world coordinate system.
     
     Assumptions:
-    All angles are in degrees.
+    curAngle is in radians.
     """
     
-    deltaX = dest.x - curPos.x
-    deltaY = dest.y - curPos.y
-    angle_rad = math.radians(curPos.theta)
-    newX = math.cos(angle_rad) * deltaX + math.sin(angle_rad) * deltaY
-    newY = math.cos(angle_rad) * deltaY - math.sin(angle_rad) * deltaX
-    newAngle = dest.theta - curPos.theta
-    return Pose2D(newX, newY, newAngle)
+    vx_b = math.cos(curAngle) * vx_w + math.sin(curAngle) * vy_w
+    vy_b = math.cos(curAngle) * vy_w - math.sin(curAngle) * vx_w
+    return vx_b, vy_b
