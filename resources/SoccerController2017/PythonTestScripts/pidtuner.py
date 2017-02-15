976cd93c963408e0ff65019004482a5b0de9eff9
lab <josephvalentine40@gmail.com>
2017-02-15 10:59:02 -0700

    Slowed down test script.

--- resources/SoccerController2017/PythonTestScripts/pidtuner.py
+++ resources/SoccerController2017/PythonTestScripts/pidtuner.py
@@ -43,9 +43,9 @@ totalTime = 3   #seconds
 sampleRate = 50 #samples per second
 #pulsePerRotation = 116.16 #New motors
 pulsePerRotation = 25000 #Old motors
-speedM1 = 2
-speedM2 = 2
-speedM3 = 2
+speedM1 = 0.5
+speedM2 = 0.5
+speedM3 = 0.5
 
 # Set the PIDQ values for all motors
 #setPID(0, 1, 1, 800)
