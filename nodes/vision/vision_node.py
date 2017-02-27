2f42b8f1d8024998570f56a41578e59d1b88678b
lab <josephvalentine40@gmail.com>
2017-02-27 14:47:09 -0700

    this works

--- nodes/vision/vision_node.py
+++ nodes/vision/vision_node.py
@@ -11,24 +11,21 @@ from std_msgs.msg import String
 
 #### Global vars ####
 
-# _img = Image()
+_img = Image()
 
-# _tmp_cnt = 0
-# def _handle_img(msg):
-   # global _tmp_cnt
+_tmp_cnt = 0
+def _handle_img(msg):
+   global _tmp_cnt
    #print "handle_img"
-   # _tmp_cnt = (_tmp_cnt + 1) % 100
+   _tmp_cnt = (_tmp_cnt + 1) % 100
    # if _tmp_cnt == 0:
    #    print 'msg:', dir(msg)
-   # global _img
-
-   
-
-   # _img = _ros2cv(msg)
+   global _img
+   _img = _ros2cv(msg)
    # _show_raw(_ros2cv(msg))
    # _ourRobot1(_ros2cv(msg))
    # _ball(_ros2cv(msg))
-   # _process_img(_ros2cv(msg))
+   _process_img(_ros2cv(msg))
    # _color_calibration(_ros2cv(msg))
 
 def _ros2cv(msg):
@@ -46,52 +43,31 @@ def _nothing(x):
     pass
 
 #publish the raw coordinate data of the objects
-def _process_img(msg):
-
-   image = _ros2cv(msg)
-   _show_raw(image)
-
-    # publish locations
-   us1_pub   = rospy.Publisher('us1', Pose2D, queue_size=10)
-   # us2_pub   = rospy.Publisher('us2', Pose2D, queue_size=10)
-   # them1_pub = rospy.Publisher('them1', Pose2D, queue_size=10)
-   # them2_pub = rospy.Publisher('them2', Pose2D, queue_size=10)
-   ball_pub  = rospy.Publisher('ball', Pose2D, queue_size=10)
-
-   fieldColor=[200,200,200,50,100,50]
+def _process_img(image):
+   fieldColor=[200,200,200,50,100,222]
    ourRobot1Color=[229,216,241,204,195,222]
    ourRobot2Color=[229,216,241,204,195,222]
    ballColor=[246, 249, 236, 215, 220, 163]
    opponent1Color=[229,216,241,204,195,222]
    opponent2Color=[229,216,241,204,195,222]
-
+   
    field = _field(image, fieldColor)
-   # ourRobot1 = _ourRobot1(image, ourRobot1Color)
-   # if ourRobot1:
-   #    us1_msg = convert_coordinates(ourRobot1[0],ourRobot1[1],ourRobot1[2], field)
-   #    us1_pub.publish(us1_msg)
-
+   ourRobot1=convert_coordinates(_ourRobot1(image, ourRobot1Color), field)
    # ourRobot2=convert_coordinates(_ourRobot2(image, ourRobot2Color), field)
-   # ball = _ball(image, ballColor)
-   # if ball:
-   #    ball_msg = convert_coordinates(ball[0], ball[1], ball[2], field)
-   #    ball_pub.publish(ball_msg)
-   
-   
+   ball=convert_coordinates(_ball(image, ballColor), field)
+   # opponent1=convert_coordinates(_opponent1(image, opponent1Color), field)
+   # opponent2=convert_coordinates(_opponent2(image, opponent2Color), field)
+
+   #publish
+
    
 
-def convert_coordinates(x,y,theta, field):
-   # This assumes the field position zero is top left
-   # field param is (x,y,w,h)
-   realW = 3.40
-   scaleW = realW / field[2]
-   realH = 2.38
-   scaleH = realH / field[3]
-   rval = Pose2D()
-   rval.x = (x - field[0])*scaleW
-   rval.y = (y - field[1])*scaleH
-   rval.theta = theta
-   return rval
+def convert_coordinates(objData, field):
+   # This assumes the field position is the bottom-left of the field.
+   # objData[0], field[0] are x-coordinates
+   # objData[1], field[1] are y-coordinates
+   # return (objData[0]-field[0], objData[1]-field[1])
+   return objData
 
 def _color_mask(image, controlWindow, color):
    # create the filter color params
@@ -155,7 +131,7 @@ def _ourRobot1(image, color):
       print(x,y,angle)
       cv2.circle(out3, (int(sortedObj[0][0]), int(sortedObj[0][1])), int(sortedObj[0][2]), (0,0,255), -1)
       cv2.circle(out3, (int(sortedObj[1][0]), int(sortedObj[1][1])), int(sortedObj[1][2]), (255,0,0), -1)
-      return (x,y,angle)
+
 
    # # show the images
    # cv2.imshow("Control", np.hstack([image, out3]))
@@ -165,7 +141,7 @@ def _ourRobot1(image, color):
 def _field(image, color):
 
    cv2.namedWindow("field")
-   # rate = rospy.Rate(100)
+   rate = rospy.Rate(100)
 
    out1 = _color_mask(image, 'field',color)
    #remove noise
@@ -190,13 +166,13 @@ def _field(image, color):
       y = sortedObj[len(objects) - 1][1]
       w = sortedObj[len(objects) - 1][2]
       h = sortedObj[len(objects) - 1][3]
-      # print(x,y,w,h)
+      print(x,y,w,h)
       cv2.rectangle(out3, (int(x), int(y)), (int(x+w), int(y+h)), (0,255,0), 2)
-      return (x,y,w,h)
+
    # # show the images
    # cv2.imshow("Control", np.hstack([image, out3]))
    cv2.imshow("field",out3)
-   # cv2.waitKey(2)
+   cv2.waitKey(2)
 
 def _ball(image, color):
 
@@ -222,12 +198,8 @@ def _ball(image, color):
   
    if len(objects) >= 1:
       sortedObj=sorted(objects,key=lambda x: x[2])
-      x=sortedObj[0][0]
-      y=sortedObj[0][1]
-      # print('ball',x,y)
-      cv2.circle(out3, (int(x), int(y)), int(sortedObj[0][2]), (100,100,255), -1)
-      return (x,y,0)
-   
+      print(objects[0][0],objects[0][1],0)
+      cv2.circle(out3, (int(sortedObj[0][0]), int(sortedObj[0][1])), int(sortedObj[0][2]), (100,100,255), -1)
    cv2.imshow("ball",out3)
    cv2.waitKey(2)
 
@@ -235,37 +207,38 @@ def main():
    rospy.init_node('ai', anonymous=False)
 
    # subscribe to camera
-   rospy.Subscriber('camera', Image, _process_img)
-   
-   # us1_pub   = rospy.Publisher('us1', Pose2D, queue_size=10)
-   # us2_pub   = rospy.Publisher('us2', Pose2D, queue_size=10)
-   # them1_pub = rospy.Publisher('them1', Pose2D, queue_size=10)
-   # them2_pub = rospy.Publisher('them2', Pose2D, queue_size=10)
-   # ball_pub  = rospy.Publisher('ball', Pose2D, queue_size=10)
-   
+   rospy.Subscriber('camera', Image, _handle_img)
+
+   # publish locations
+   us1_pub   = rospy.Publisher('us1', Pose2D, queue_size=10)
+   us2_pub   = rospy.Publisher('us2', Pose2D, queue_size=10)
+   them1_pub = rospy.Publisher('them1', Pose2D, queue_size=10)
+   them2_pub = rospy.Publisher('them2', Pose2D, queue_size=10)
+   ball_pub  = rospy.Publisher('ball', Pose2D, queue_size=10)
+
    rate = rospy.Rate(100) # 100 Hz
    while not rospy.is_shutdown():
 
       # Get a message ready to send
-      # us1_msg   = Pose2D()
-      # us2_msg   = Pose2D()
-      # them1_msg = Pose2D()
-      # them2_msg = Pose2D()
-      # ball_msg  = Pose2D()
+      us1_msg   = Pose2D()
+      us2_msg   = Pose2D()
+      them1_msg = Pose2D()
+      them2_msg = Pose2D()
+      ball_msg  = Pose2D()
 
-      # # do vision stuff
-      # us1_msg.x, us1_msg.y, us1_msg.theta       = -0.25, 0.0, 0.0
-      # us2_msg.x, us2_msg.y, us2_msg.theta       = -0.5, 0.0, 0.0
-      # them1_msg.x, them1_msg.y, them1_msg.theta = 0.25, 0.0, 180.0
-      # them2_msg.x, them2_msg.y, them2_msg.theta = 0.5, 0.0, 180.0
-      # ball_msg.x, ball_msg.y, ball_msg.theta    = 0.0, 0.0, 0.0
+      # do vision stuff
+      us1_msg.x, us1_msg.y, us1_msg.theta       = -0.25, 0.0, 0.0
+      us2_msg.x, us2_msg.y, us2_msg.theta       = -0.5, 0.0, 0.0
+      them1_msg.x, them1_msg.y, them1_msg.theta = 0.25, 0.0, 180.0
+      them2_msg.x, them2_msg.y, them2_msg.theta = 0.5, 0.0, 180.0
+      ball_msg.x, ball_msg.y, ball_msg.theta    = 0.0, 0.0, 0.0
 
-      # # publish
-      # us1_pub.publish(us1_msg)
-      # us2_pub.publish(us2_msg)
-      # them1_pub.publish(them1_msg)
-      # them2_pub.publish(them2_msg)
-      # ball_pub.publish(ball_msg)
+      # publish
+      us1_pub.publish(us1_msg)
+      us2_pub.publish(us2_msg)
+      them1_pub.publish(them1_msg)
+      them2_pub.publish(them2_msg)
+      ball_pub.publish(ball_msg)
 
       # Wait however long it takes to make this tick at 100Hz
       rate.sleep()
