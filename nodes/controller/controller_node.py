110ac01606fdc06c13851f6f431a957fba200be8
steven93 <stwhit93@gmail.com>
2017-02-28 12:12:31 -0700

    Put the estimator code into the controller node.

--- nodes/controller/controller_node.py
+++ nodes/controller/controller_node.py
@@ -11,7 +11,15 @@ import Controller
 
 _ctrl_period = 1.0/100
 
-# My current state
+# Flag to indicate when a message is received from vision
+_msg_received = False
+
+# My measured state
+_xmeas = 0
+_ymeas = 0
+_thetameas = 0
+
+# My estimated state
 _xhat = 0
 _yhat = 0
 _thetahat = 0
@@ -19,12 +27,13 @@ _thetahat = 0
 # -------------------
 
 def _handle_me(msg):
-    global _xhat, _yhat, _thetahat
+    global _xmeas, _ymeas, _thetameas
     # print 'xhat: {}\nyhat: {}\nthetahat: {}'.format(
     #     _xhat, _yhat, _thetahat)
-    _xhat = msg.x
-    _yhat = msg.y
-    _thetahat = msg.theta
+    _xmeas = msg.x
+    _ymeas = msg.y
+    _thetameas = msg.theta
+    _msg_received = True
 
 
 def _handle_desired_position(msg):
@@ -32,10 +41,25 @@ def _handle_desired_position(msg):
     Controller.set_commanded_position(msg.x, msg.y, msg.theta)
 
 
+def _estimate_state(vx, vy, w):
+    global _xhat, _yhat, _thetahat, _msg_received
+    
+    _xhat = _xhat + _ctrl_period * vx
+    _yhat = _yhat + _ctrl_period * vy
+    _thetahat = _thetahat + _ctrl_period * w
+    
+    if _msg_received:
+        _xhat = _alpha * _xhat + (1 - _alpha) * _xmeas
+        _yhat = _alpha * _yhat + (1 - _alpha) * _ymeas
+        _thetahat = _alpha * _thetahat + (1 - _alpha) * _thetameas
+        
+        _msg_received = False
+
+
 def main():
     rospy.init_node('controller', anonymous=False)
 
-    # Subscribe to my current state (from the vision node)
+    # Subscribe to my measured state (from the vision node)
     # and my desired state (from the ai node)
     rospy.Subscriber('me', Pose2D, _handle_me)
     rospy.Subscriber('desired_position', Pose2D, _handle_desired_position)
@@ -45,9 +69,16 @@ def main():
 
     # initialize the controller
     Controller.init()
+    
+    # Initialize commanded velocities
+    vx = 0
+    vy = 0
+    w = 0
 
     rate = rospy.Rate(int(1/_ctrl_period))
     while not rospy.is_shutdown():
+        
+        _estimate_state(vx, vy, w)
 
         (vx, vy, w) = Controller.update(_ctrl_period, _xhat, _yhat, _thetahat)
 
